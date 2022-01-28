de6aa5e9c661918f491c1ca8d89e415e41b41770
bcrist3 <bcrist3@huskers.unl.edu>
2022-01-28 16:17:18 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,15 +1,18 @@
 /**
  * Author: Ben Crist
- *         
+ *
  * Date: 2022/01/28
  *
  * A simple hello world program in C
  *
  */
+#include <stdlib.h>
+#include <stdio.h>
+
 int main(int argc, char **argv) {
 
-  printf("Hello World!");
+  printf("Hello World, I'm Ben Crist!\n");
+  printf("My major is Computer Engineering!\n");
 
   return 0;
 }
-gu
