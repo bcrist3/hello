762b0031f4d4bfe6d88002ba41c43dd9fdbee83e
Ben <bcrist3@huskerz.unl.edu>
2022-01-28 15:12:11 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Ben Crist
+ *         
+ * Date: 2022/01/28
+ *
+ * A simple hello world program in C
+ *
+ */
+int main(int argc, char **argv) {
+
+  printf("Hello World!");
+
+  return 0;
+}
